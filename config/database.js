151cecca0d282c05dc8ebda0d7526c4513b492a5
azul182 <84513231+azul182@users.noreply.github.com>
2022-08-31 11:45:03 -0300

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,13 +1,17 @@
-const moongose = require('mongoose')
+const mongoose = require('mongoose')
 
 const conexao = async()=>{
-    const atlas = await mongoose.connect('mongodb+srv://user3tri:thi020206@cluster0.s3hds.mongodb.net/test')
+    const atlas = await mongoose.connect('mongodb+srv://user3tri:thi020206@cluster0.s3hds.mongodb.net/html5up')
 }
 
-const modelo = moongose.Schema({
+const modelo = mongoose.Schema({
     estilo:String,
     imagem:String,
     titulo:String,
     texto:String,
     publicado:{type:Date, default:Date.now}
 })
+
+const conteudo = mongoose.model('conteudo', modelo)
+
+module.exports = {conexao, conteudo}
--- routes/formulario.js
+++ routes/formulario.js
@@ -1,9 +1,19 @@
+const {conexao, conteudo} = require('../config/database')
+
 module.exports = (app) =>{
     app.get('/formulario',(req,res)=>{
         res.render('formulario.ejs')
     })
 
-    app.post('/formulario',(req,res) =>{
-        res.send(req.body)
+    app.post('/formulario',async(req,res) =>{
+        const bode = req.body
+        conexao()
+        const documento = await new conteudo({
+            estilo:bode.estilo,
+            imagem:bode.imagem,
+            titulo:bode.titulo,
+            texto:bode.texto    
+        }).save()
+        res.redirect('/formulario')
     })
-}
+} 
